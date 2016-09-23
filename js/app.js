7cd0dd2741bb2d01bcba858b5ed1b4a3aada74eb
Christian Hensen <christian.j.hensen@gmail.com>
2016-09-22 20:01:27 -0400

    animation working properly on focus and blur

---
+++ js/app.js
@@ -0,0 +1,17 @@
+var formInputs = document.getElementsByTagName('input');
+
+
+var addAnimationClass = function() {
+  this.previousElementSibling.classList.add('animate-label');
+}
+
+var removeAnimationClass = function(inputElement) {
+  if (this.value === "") {
+    this.previousElementSibling.classList.remove('animate-label');
+  }
+}
+
+for (var i = 0; i < formInputs.length-1; i++) {
+  formInputs[i].addEventListener("focus", addAnimationClass);
+  formInputs[i].addEventListener("blur", removeAnimationClass);
+}
