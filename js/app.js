2391daae607cd47ac5f95e8fd069f7be274adf31
Christian Hensen <christian.j.hensen@gmail.com>
2016-09-22 22:34:56 -0400

    added working indicators for password requirements (length, uppercase, number, special character)

--- js/app.js
+++ js/app.js
@@ -3,6 +3,10 @@ var SignupForm = (function() {
   var form = {};
 
   var formInputs = document.getElementsByTagName('input');
+  var passwordInput = document.getElementById('password');
+  var passwordIndicator = document.getElementsByClassName('passwordIndicator');
+  console.log(passwordIndicator);
+  var confirmPasswordInput = document.getElementById('confirmPassword');
 
   var addAnimationClass = function() {
     this.previousElementSibling.classList.add('animate-label');
@@ -18,6 +22,55 @@ var SignupForm = (function() {
     formInputs[i].addEventListener("focus", addAnimationClass);
     formInputs[i].addEventListener("blur", removeAnimationClass);
   }
+
+  var checkPasswordLength = function() {
+    return passwordInput.value.length > 8;
+  };
+
+  var checkPasswordCase = function() {
+    var upperCaseChars = /[A-Z]/;
+    return upperCaseChars.test(passwordInput.value);
+
+  };
+
+  var checkPasswordNum = function() {
+    var nums = /[1-9]/;
+    return nums.test(passwordInput.value);
+  };
+
+  var checkPasswordSpecialChar = function() {
+    var specialChars = /^[a-zA-Z0-9- ]*$/;
+    return !specialChars.test(passwordInput.value);
+  };
+
+  var isPasswordValid = function() {
+    if(checkPasswordLength()) {
+      passwordIndicator[0].style.display="inline";
+    } else {
+      passwordIndicator[0].style.display="none";
+    }
+
+    if(checkPasswordCase()) {
+      passwordIndicator[1].style.display="inline";
+    } else {
+      passwordIndicator[1].style.display="none";
+    }
+
+    if(checkPasswordNum()) {
+      passwordIndicator[2].style.display="inline";
+    } else {
+      passwordIndicator[2].style.display="none";
+    }
+
+    if(checkPasswordSpecialChar()) {
+      passwordIndicator[3].style.display="inline";
+    } else {
+      passwordIndicator[3].style.display="none";
+    }
+  };
+
+  passwordInput.addEventListener("focus", isPasswordValid);
+  passwordInput.addEventListener("keyup", isPasswordValid);
 
   return form;
 
